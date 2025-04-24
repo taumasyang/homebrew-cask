d06cb10c6432893ed2a8901a8d7573ee6b6b5a01
tauyoung <tauyoung@icloud.com>
2025-04-24 14:30:00 -0500

    Yaagl 0.2.22

---
+++ Casks/yaagl-hsr.rb
@@ -0,0 +1,33 @@
+cask "yaagl-hsr" do
+  version "0.2.22"
+  sha256 "a3da426b13074c25b8e6ba40b5365dc3ed312920a01000882e5009abeb5bca24"
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.app.tar.gz"
+  name "Yaagl HSR"
+  desc "yet another anime game launcher"
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
+
+  livecheck do
+    url :url
+    strategy :github_releases
+  end
+
+  auto_updates true
+  if Hardware::CPU.intel?
+    depends_on macos: '>= :high_sierra'
+  elsif Hardware::CPU.arm?
+    depends_on macos: '>= :sonoma'
+  end
+
+  app "Yaagl HSR.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yaagl HSR",
+    "~/Library/Caches/com.3shain.yaagl.hkrpg.cn",
+    "~/Library/WebKit/com.3shain.yaagl.hkrpg.cn",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
