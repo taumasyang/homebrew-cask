72ee54d6da83b10fec8a2fdefc62c4918db693d1
tauyoung <tauyoung@icloud.com>
2025-04-24 14:30:00 -0500

    WeChat 4.0.3

---
+++ Casks/wechat.rb
@@ -0,0 +1,39 @@
+cask "wechat" do
+  version "4.0.3"
+  sha256 :no_check
+
+  url "https://dldir1.qq.com/weixin/Universal/Mac/WeChatMac.dmg"
+  name "WeChat for Mac"
+  name "微信 Mac 版"
+  desc "Free messaging and calling application"
+  homepage "https://mac.weixin.qq.com/"
+
+  livecheck do
+    url "https://dldir1.qq.com/weixin/mac/mac-release.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "WeChat.app"
+
+  uninstall quit: "com.tencent.xinWeChat"
+
+  zap trash: [
+    "~/Library/Application Scripts/$(TeamIdentifierPrefix)com.tencent.xinWeChat",
+    "~/Library/Application Scripts/$(TeamIdentifierPrefix)com.tencent.xinWeChat.IPCHelper",
+    "~/Library/Application Scripts/com.tencent.xinWeChat",
+    "~/Library/Application Scripts/com.tencent.xinWeChat.MiniProgram",
+    "~/Library/Application Scripts/com.tencent.xinWeChat.WeChatMacShare",
+    "~/Library/Caches/com.tencent.xinWeChat",
+    "~/Library/Containers/$(TeamIdentifierPrefix)com.tencent.xinWeChat.IPCHelper",
+    "~/Library/Containers/com.tencent.xinWeChat",
+    "~/Library/Containers/com.tencent.xinWeChat.MiniProgram",
+    "~/Library/Containers/com.tencent.xinWeChat.WeChatMacShare",
+    "~/Library/Cookies/com.tencent.xinWeChat.binarycookies",
+    "~/Library/Group Containers/$(TeamIdentifierPrefix)com.tencent.xinWeChat",
+    "~/Library/Preferences/com.tencent.xinWeChat.plist",
+    "~/Library/Saved Application State/com.tencent.xinWeChat.savedState",
+  ]
+end
