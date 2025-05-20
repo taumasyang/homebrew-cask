b09808eb323319177a5d92ffe0ba4856f925c820
tauyoung <tauyoung@icloud.com>
2025-05-20 07:15:00 -0500

    Style fix

--- Casks/yaagl-hsr.rb
+++ Casks/yaagl-hsr.rb
@@ -2,9 +2,16 @@ cask "yaagl-hsr" do
   version "0.2.22"
   sha256 "a3da426b13074c25b8e6ba40b5365dc3ed312920a01000882e5009abeb5bca24"
 
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :high_sierra"
+  end
+
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.app.tar.gz"
   name "Yaagl HSR"
-  desc "yet another anime game launcher"
+  desc "Yet another anime game launcher"
   homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
 
   livecheck do
@@ -13,11 +20,6 @@ cask "yaagl-hsr" do
   end
 
   auto_updates true
-  if Hardware::CPU.intel?
-    depends_on macos: '>= :high_sierra'
-  elsif Hardware::CPU.arm?
-    depends_on macos: '>= :sonoma'
-  end
 
   app "Yaagl HSR.app"
 
--- Casks/yaagl.rb
+++ Casks/yaagl.rb
@@ -2,9 +2,16 @@ cask "yaagl" do
   version "0.2.22"
   sha256 "52951b2198404a920e2e24d84ea1e101f075924d86e2195d3246cbe5ef77569e"
 
+  on_arm do
+    depends_on macos: ">= :sonoma"
+  end
+  on_intel do
+    depends_on macos: ">= :high_sierra"
+  end
+
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.app.tar.gz"
   name "Yaagl"
-  desc "yet another anime game launcher"
+  desc "Yet another anime game launcher"
   homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
 
   livecheck do
@@ -13,11 +20,6 @@ cask "yaagl" do
   end
 
   auto_updates true
-  if Hardware::CPU.intel?
-    depends_on macos: '>= :high_sierra'
-  elsif Hardware::CPU.arm?
-    depends_on macos: '>= :sonoma'
-  end
 
   app "Yaagl.app"
 
